2032555c1d8a16bba66f7066ad450f2a1d4984ee
UselessPants <46540898+UselessPants@users.noreply.github.com>
2024-03-17 02:41:59 -0400

    none

---
+++ Assets/Cainos/DefaultCharacter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultCharacter : MonoBehaviour
+{
+    int attack;
+    int defense;
+    int movementSpeed;
+    int ballSpeed;
+    int hitPoints;
+    int ballPoints;
+    int blocking;
+    int level;
+    int skillPoints;
+    int[][] skillTree;
+    int throwing;
+    int[][] coordinates;
+    bool hasBall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
